1133af77b4e43478401bfdd81564e0d1b3eb1c45
Dmitry Karamin <karamin.work@gmail.com>
2017-03-19 00:30:29 +0300

    Post image from local path

--- facebook/admin.py
+++ facebook/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.conf import settings
+import imghdr
 
 import requests
 
@@ -15,9 +16,14 @@ class ArticleAdmin(admin.ModelAdmin):
         for x in form.instance.post_to.all():
 
             if x.page_id:
+                #
+                files = form.instance.picture
+                # print dir(files.)
+                print files.name.split("/")[-1]
+
+                filename = files.name.split("/")[-1]
 
                 payload = {
-                    'url': 'https://upload.wikimedia.org/wikipedia/en/f/f9/No-image-available.jpg',
                     'caption': form.instance.text,
                     'access_token': x.page_access_token
                 }
@@ -25,8 +31,24 @@ class ArticleAdmin(admin.ModelAdmin):
                 url = u'https://graph.facebook.com/v2.8/{}/photos'.format(x.page_id)
 
                 print url
-
-                r = requests.post(url, data=payload)
-
+                print "image format", imghdr.what(form.instance.picture)
+                #
+                # with open(form.instance.picture, 'r') as picture:
+                #
+                files = [
+                            (
+                                'images',
+                                ("No-image-available_HSqc8G9.jpg", open(form.instance.picture.path, 'rb'),
+                                'image/{0}'.format(imghdr.what(form.instance.picture)))
+                             )
+                ]
+                #
+                r = requests.post(url, files=files, data=payload)
+                #
                 print r.text
                 print r.status_code
+
+
+# >>> multiple_files = [
+#         ('images', ('foo.png', open('foo.png', 'rb'), 'image/png')),
+#         ('images', ('bar.png', open('bar.png', 'rb'), 'image/png'))]
